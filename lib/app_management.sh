0b5c15e97276ade74c8c75416ecb6ca0d07a9d32
rolivieri <roliv@us.ibm.com>
2017-01-09 14:08:49 -0600

    Adding remote debug support.

--- lib/app_management.sh
+++ lib/app_management.sh
@@ -91,6 +91,7 @@ function copyLLDBServer() {
   ls -la $BUILD_DIR/.swift-bin
   echo "COPIED LLDBD!!!!!!!"
   find $CACHE_DIR/$SWIFT_NAME_VERSION -name "lldb" -type f -perm /a+x
+  ls -la $CACHE_DIR/$SWIFT_NAME_VERSION
   ls -la $CACHE_DIR/$SWIFT_NAME_VERSION/usr/bin
   echo "ANYTHING HERE???"
   #TO BE REMOVED
