57c9e6c188be0f53c5787be9154129d8b34d96f4
Christian Compton <cbcompto@us.ibm.com>
2017-04-11 12:02:58 -0500

    cf-cli 6.26.0 support

--- ci/performance_validator.sh
+++ ci/performance_validator.sh
@@ -70,11 +70,11 @@ passed_repush=$?
 cd ..
 
 # Verify 200 from application route status code
-cf app Kitura-Starter
-url=$(cf app Kitura-Starter | grep routes:)
-echo "Obtained application route assignment: $url"
+cf app $APPLICATION_DIR
+url=$(cf app $APPLICATION_DIR | grep routes:)
+echo "Obtained $APPLICATION_DIR route assignment: $url"
 status=$(curl -s -o /dev/null -w '%{http_code}' ${url#routes: })
-echo "cURL application route status:  $status"
+echo "cURL $APPLICATION_DIR route status:  $status"
 [ "$status" = 200 ] ; url_success=$?
 
 ! (( $passed | $passed_repush | $url_success ));
